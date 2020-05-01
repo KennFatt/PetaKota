11ca09b5a209eced7e32293ed00efabde510a9e5
KennFatt <clashofklen@gmail.com>
2020-05-01 17:02:46 +0700

    Add clarity on

--- app/src/main/java/xyz/kennan/petakota/Utility.kt
+++ app/src/main/java/xyz/kennan/petakota/Utility.kt
@@ -5,6 +5,13 @@ import android.widget.Toast
 
 class Utility {
     companion object {
+
+        /**
+         * TODO: Remove this debug method in production
+         *
+         * Since my Logcat is always empty, Toast is the best and easy way to
+         *  debugging.
+         */
         public fun d(ctx: Context, m: String) {
             Toast.makeText(ctx, m, Toast.LENGTH_SHORT).show()
         }
